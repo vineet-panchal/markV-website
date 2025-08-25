bc7026eab789cfeb75808fd9940c83b2da3c3c6a
Vineet Panchal <vineetpanchal03@gmail.com>
2025-08-25 05:44:34 -0400

    Add AboutMe component to About page
    Introduces a new AboutMe component with personal information, contact links, and custom styling. Integrates AboutMe into the /about page, replacing static content and enhancing the page's design and interactivity.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Navbar from '@/components/navbar';
+import AboutMe from '@/components/about-page/aboutme';
 
 export default function Page() {
   return (
     <>
-      <Navbar />
-      About Page
+      <div className="bg-[var(--color-midnight)]">
+        <Navbar />
+        <AboutMe />
+        About Page
+      </div>
     </>
   )
 };
